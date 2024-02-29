a68ca0de8013197755ab991a9db2c3611493e3ef
cocktailpeanut <cocktailpeanuts@proton.me>
2024-02-29 11:18:38 -0500

    update

--- start.js
+++ start.js
@@ -1,6 +1,16 @@
 module.exports = {
   "daemon": true,
   "run": [{
+    "method": "modal",
+    "params": {
+      "title": "Ollama",
+      "description": "Launch Ollama before proceeding.",
+      "menu": [{
+        "text": "Install Ollama",
+        "href": "https://ollama.com/"
+      }]
+    }
+  }, {
     "method": "shell.run",
     "params": {
       "path": "app/backend",
